f078590ba0fe01b59f5b47faa5a8b64a18e7e76a
Emily Su <emilyzqsu@gmail.com>
2025-05-09 13:23:15 -0400

    Pushed initial files for paper

---
+++ README.md
@@ -0,0 +1,17 @@
+# Data Quality (Work in Progress Title)
+
+## Overview
+
+## File Structure
+
+The repo is structured as:
+-   `data/00-simulated_data` contains data obtained from simulation of the ideal dataset for our analysis.
+-   `data/01-raw_data` contains the raw data as obtained from Open Data Toronto.
+-   `data/02-analysis_data` contains the cleaned dataset in CSV form that was constructed and used in our analysis.
+-   `other` contains sketches.
+-   `paper` contains the files used to generate the paper, including the Quarto document and reference bibliography file, as well as the PDF of the paper. 
+-   `scripts` contains the Python scripts used to install, simulate, download, clean, explore, and test our data.
+
+## Statement on LLM usage
+
+No LLMs were used for any aspect of this work.
---
+++ scripts/02-download_data.py
@@ -0,0 +1,33 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from Open Data Toronto
+# Author: Emily Su
+# Date: 7 May 2025
+# Contact: em.su@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: Have the required packages ('requests', 'csv', and 'io') installed.
+
+
+#### Workspace setup ####
+import requests
+import csv
+from io import StringIO
+
+#### Download data ####
+# REFERENCE: From the Open Data Toronto website
+url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/package_show"
+params = { "id": "catalogue-quality-scores"} # Contains the dataset we want to retrieve 
+package = requests.get(url, params = params).json()
+ 
+# Get resource data 
+for idx, resource in enumerate(package["result"]["resources"]):
+    # for datastore_active resources:
+    if resource["datastore_active"]:
+        # To get all records in CSV format:
+        url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/datastore/dump/" + resource["id"]
+        
+        # Referenced: https://stackoverflow.com/questions/42834861/csv-file-from-string-output
+        #### Save data as a CSV ####
+        reader = csv.reader(StringIO(requests.get(url).text), skipinitialspace=True)
+        with open('data/01-raw_data/raw_data.csv', 'w') as output_file:
+            writer = csv.writer(output_file)
+            writer.writerows(reader) # Write in rows into the new file
